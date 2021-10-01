40267ea22ca73ab79dfd308f86d98aeb8b9e8d56
F.T <ttyn4519@outlook.jp>
2021-10-02 04:22:09 +0900

    Update classification.py

--- classification.py
+++ classification.py
@@ -35,3 +35,95 @@ testing_ds = tf.keras.preprocessing.image_dataset_from_directory(
 	)
 
 class_names = training_ds.class_names
+
+plt.figure(figsize=(10,10))
+for images,labels in training_ds.take(1):
+	for i in range(12):
+		ax = plt.subplot(3,4,i+1)
+		plt.imshow(images[i].numpy().astype('uint8'))
+		plt.title(class_names[labels[i]])
+		plt.grid(True)
+
+## healthy vs unhealthy samples in data set
+healthy = os.listdir('path/rice_plant_img/healthy_images')
+unhealthy = os.listdir('path/rice_plant_img/unhealthy_images')
+plt.pie ([len(healthy), len(unhealthy)], labels = ['Healthy rice Plants', 'Unhealthy rice Plants'],autopct='%1.2f%%')
+plt.title('healthy vs Unhealthy samples in data set')
+
+
+## defining data augumentation layer
+data_augmentation = tf.keras.Sequential([
+  layers.experimental.preprocessing.RandomFlip("horizontal_and_vertical"),
+  layers.experimental.preprocessing.RandomRotation(0.2),
+  
+  
+])
+
+
+#define CNN
+MyCnn = tf.keras.models.Sequential([
+  layers.experimental.preprocessing.Rescaling(1./255),
+  layers.Conv2D(32, 3, activation='relu'),
+  layers.MaxPooling2D(),
+  layers.Conv2D(64, 3, activation='relu'),
+  layers.MaxPooling2D(),
+  layers.Conv2D(128, 3, activation='relu'),
+  layers.MaxPooling2D(),
+  layers.Flatten(),
+  layers.Dense(256, activation='relu'),
+  layers.Dropout(0.25),
+  layers.Dense(1, activation= 'sigmoid')
+])
+
+
+MyCnn.compile(optimizer=tf.keras.optimizers.Adam(learning_rate = 0.001),loss='binary_crossentropy', metrics=['accuracy'])
+
+
+#rescheduler
+
+def scheduler(epoch,lr):
+	if epoch<5:
+		return lr
+	else:
+		return lr * tf.math.exp(-0.1)
+
+callback = tf.keras.callbacks.LearningRateScheduler(scheduler)
+
+#training model
+retVal = MyCnn.fit(training_ds,validation_data=testing_ds,callbacks=[callback],epochs=20)
+
+plt.plot(retVal.history['loss'],label='training loss')
+plt.plot(retVal.history['accuracy'],label='training accuracy')
+plt.legend()
+plt.grid(True)
+
+plt.plot(retVal.history['val_loss'],label='validation loss')
+plt.plot(retVal.history['val_accuracy'],label='validation accuracy')
+plt.legend()
+plt.grid(True)
+
+
+AccuracyVector=[]
+plt.figure(figsize=(30,20))
+for images,labels in testing_ds.take(1):
+	predictions = MyCnn.predict(images)
+	predlabel=[]
+	prdlbl=[]
+
+	for mem in predictions:
+		predlabel.append(class_names[int(round(mem[0]))])
+		prdlbl.append(np.argmax(mem))
+	
+	AccuracyVector=np.array(prdlbl)==labels
+
+	for i in range(20):
+		ax=plt.subplot(5,4,i+1)
+		plt.imshow(images[i].numpy().astype('uint8'))
+		plt.title('Pred:'+predlabel[i]+' actl:'+class_names[labels[i]])
+		plt.axis('off')
+		plt.grid(True)
+
+from keras.utils.vis_utils import plot_model
+plot_model(MyCnn, to_file='model_plot.png', show_shapes=True, show_layer_names=True)
+
+MyCnn.save('model1.h5')
